173a7abb9e57b589868461d56785a6909ff09a19
“jasonjiangstuy” <jasonjiangny@gmail.com>
2022-06-07 22:39:55 -0400

    homepage layout works

--- gui.py
+++ gui.py
@@ -25,22 +25,23 @@ def init():
   global ws
   ws = Tk()
   ws.title("Poki-lator")
-  
+  # ws.attributes('-fullscreen', True)
+
   #Get the current screen width and height of computer // not accurate
   global screen_width, screen_height
   screen_width = ws.winfo_screenwidth()
   screen_height = ws.winfo_screenheight()
-
-
+  # screen_width = 2000
+  # screen_height = 1000  
   ws.geometry('%dx%d+%d+%d' % (screen_width, screen_height, 0, 0))
   
   print(f"{screen_width}, {screen_height}")
   #init main drawable canvas
   canvas = Canvas(ws, width=screen_width, height=screen_height, bg='yellow')
-  canvas.pack(anchor = CENTER)
+  canvas.pack(fill=Y, side=TOP, expand=True, anchor = CENTER)
   # start off in homepage
   home(canvas)
-
+ 
 def home(target):
   title = Text(target, width=screen_width, height=1)
   title.tag_configure("Title", justify='center')
@@ -51,48 +52,44 @@ def home(target):
   title.tag_add("Title", "1.0", "end")
 
   # image slideshow layer
-  
-  slideshow_base = Canvas(target, width = screen_width//2, height = screen_height//2)   
+  slideshow_ratio = 2/3
+  slideshow_base = Canvas(target, width = screen_width * slideshow_ratio, height = screen_height *slideshow_ratio)   
   slideshow_base.configure(bg='blue')   
   
   # testing centering + image rendering
   img = PhotoImage(file="Pics/poki-Pics/POKI.png")
   im = img
   slideshow_base.pack(anchor=CENTER)  
+  slideshow_base.place(anchor=CENTER, x=screen_width /2, y=screen_height/2)
   ws.update()
   slideshow_base.create_image(slideshow_base.winfo_width()//2, slideshow_base.winfo_height()//2, anchor=CENTER, image=im)   
   # a and b values of the slideshow oval
-  slideshow_a = slideshow_base.winfo_width()
-  slideshow_b = slideshow_base.winfo_height()
-  slideshow_base.create_oval(0, 0, slideshow_a, slideshow_b)
+  oval_margin_percentage = .2
+  slideshow_a = slideshow_base.winfo_width() * (1-2*oval_margin_percentage)
+  slideshow_b = slideshow_base.winfo_height() * (1-2*oval_margin_percentage)
+  slideshow_base.create_oval(slideshow_base.winfo_width()*oval_margin_percentage, slideshow_base.winfo_height()*oval_margin_percentage, slideshow_base.winfo_width()*(1-oval_margin_percentage),  slideshow_base.winfo_height()*(1-oval_margin_percentage))
   # image will be centered in the center of the page
   # print('%d, %d' % (canvas.winfo_width()//2, canvas.winfo_height()//2))
 
-  
   delta_theta = 2 * math.pi / len(home_order)
   streamer_index = 0
-  starting_theta = -3/2 * math.pi
-
-  # error
-  # poki image disappears
-  # images dont show up , only one button shows up at the lower right corner
+  starting_theta = -3/2 * math.pi # start at downwards angle
+  starting_y = slideshow_b/2 * math.sin(delta_theta * streamer_index + starting_theta) + slideshow_base.winfo_height()/2 
   for streamer_name in home_order:
     # find location along the elipse
-    image_x = slideshow_a/2 * math.cos(delta_theta * streamer_index + starting_theta) + slideshow_a/2 
-    image_y = slideshow_b/2 * math.sin(delta_theta * streamer_index + starting_theta) + slideshow_b/2 
-
-    # test drawing a circle object at each image locations
-    # slideshow_base.create_oval(image_x-25, image_y-25, image_x+25, image_y+25, fill="red")
-    
-    
-    # Marker
-    # # render image according to streamer name
-    #   # Read the Image
+    image_x = slideshow_a/2 * math.cos(delta_theta * streamer_index + starting_theta) + slideshow_base.winfo_width()/2 
+    image_y = slideshow_b/2 * math.sin(delta_theta * streamer_index + starting_theta) + slideshow_base.winfo_height()/2 
+    # render image according to streamer name
     print(f"Pics/{streamer_name}-Pics/home.png" )
     image = Image.open(f"Pics/{streamer_name}-Pics/home.png")
-      # Resize the image
-    resize_image = image.resize((40, 60))
-        # resize_image.show() # test what the image looks like
+    # Resize the image based on y distance from bottom of screen
+    y_dist = (starting_y - image_y + 1) / 60
+    decay_constant = 1.15
+    print(f"y_dist {y_dist}")
+    image_dimensions = (int(400 * pow(decay_constant, -y_dist)), int(600 * pow(decay_constant, -y_dist)))
+    print("%d, %d" % image_dimensions)
+
+    resize_image = image.resize( image_dimensions ) 
     img = ImageTk.PhotoImage(resize_image)
 
     # test the computer locations along the ellipse
@@ -102,13 +99,9 @@ def home(target):
     
     btn = Button(slideshow_base, text = 'Click Me !', command=buttonPressed, image = img)
     btn.image = img
-    btn.place(x=image_x, y=image_y - 50)
-
-    # just image 
-    # slideshow_base.create_image(image_x, image_y, anchor=CENTER, image=img)   
+    btn.place(x=image_x, y=image_y, anchor=CENTER)
     streamer_index += 1
-  # error 45 
-  
+ 
   ws.mainloop()
 
   
