1054e7cd4c6e36377b410fcc95018e6db0733e40
“jasonjiangstuy” <jasonjiangny@gmail.com>
2022-06-13 07:50:31 -0400

    working on go button + sketching out go screen

--- gui.py
+++ gui.py
@@ -1,13 +1,10 @@
-from calendar import c
-from cmath import pi
 from tkinter import *
 from tkinter import ttk
 import math
-from PIL import Image, ImageTk
-from functools import partial
+from PIL import Image, ImageTk, ImageOps, ImageDraw
 
-import ourBaseFunc
-errorBound = 0.00001
+from functools import partial
+import listFunctions
 
 # vision - poki slide show, each poki mood = a function
 # sub moods for catagories 
@@ -55,10 +52,10 @@ def home(target):
   title.tag_add("Title", "1.0", "end")
 
   # image slideshow layer
-  slideshow_ratio = 2/3
+  slideshow_ratio = 3/4 # how big of the width and heigh should the main area be
   slideshow_base = Canvas(target, width = screen_width * slideshow_ratio, height = screen_height *slideshow_ratio)   
   slideshow_base.configure(bg='blue')   
-  
+  slideshow_offset_y = -screen_height * slideshow_ratio * 1/9
   slideshow_base.pack(anchor=CENTER)  
   slideshow_base.place(anchor=CENTER, x=screen_width /2, y=screen_height/2)
   ws.update()
@@ -66,43 +63,78 @@ def home(target):
   oval_margin_percentage = .2
   slideshow_a = slideshow_base.winfo_width() * (1-2*oval_margin_percentage)
   slideshow_b = slideshow_base.winfo_height() * (1-2*oval_margin_percentage)
-  slideshow_base.create_oval(slideshow_base.winfo_width()*oval_margin_percentage, slideshow_base.winfo_height()*oval_margin_percentage, slideshow_base.winfo_width()*(1-oval_margin_percentage),  slideshow_base.winfo_height()*(1-oval_margin_percentage))
   # image will be centered in the center of the page
   # print('%d, %d' % (canvas.winfo_width()//2, canvas.winfo_height()//2))
 
   delta_theta = 2 * math.pi / len(home_order)
   streamer_index = 0
   starting_theta = -3/2 * math.pi # start at downwards angle
-  starting_y = slideshow_b/2 * math.sin(delta_theta * streamer_index + starting_theta) + slideshow_base.winfo_height()/2 
+  first = True
   streamer_buttons = []
   streamer_images = []
+  directional_arrows = []
   for streamer_name in home_order:
     # find location along the elipse
     image_x = slideshow_a/2 * math.cos(delta_theta * streamer_index + starting_theta) + slideshow_base.winfo_width()/2 
     image_y = slideshow_b/2 * math.sin(delta_theta * streamer_index + starting_theta) + slideshow_base.winfo_height()/2 
-    # render image according to streamer name
-    print(f"Pics/{streamer_name}-Pics/home.png" )
-    image = Image.open(f"Pics/{streamer_name}-Pics/home.png")
-    # (first time)add to streamer_images
-    streamer_images.append(image)
-    # Resize the image based on y distance from bottom of screen
-    y_dist = (starting_y - image_y + 1) / 60
-    decay_constant = 1.15
-    print(f"y_dist {y_dist}")
     image_max_x = 300
     image_max_y = 500
-    image_dimensions = (int(image_max_x * pow(decay_constant, -y_dist)), int(image_max_y * pow(decay_constant, -y_dist)))
-    
+    if first:
+      starting_y = image_y
+      starting_x = image_x
+
+
+
+
+    # render image according to streamer name
+    image = Image.open(f"Pics/{streamer_name}-Pics/home.png")
+    # (first time)add to streamer_images
+    # Resize the image based on y distance from bottom of screen
+    y_dist = (starting_y - image_y + 1) / 60
+    decay_constant = 1.08
+    decay_factor = pow(decay_constant, -y_dist)
+
+
+    image_width = int(image_max_x * decay_factor)
+    image_height = int(image_max_y * decay_factor)
+    image_dimensions = (image_width, image_height)
+    name_tage = Label(slideshow_base, text=streamer_name, width=len(streamer_name), height=1)
+    if (first):
+      name_tage.place(anchor=CENTER, x=image_x - image_width/2 , y=image_y)
+      name_tage.config(state=DISABLED)
+
     print("%d, %d" % image_dimensions)
+    max_image_dimensions = (image_max_x, image_max_y)
+    resize_image = image.resize( max_image_dimensions)
 
+    mask = Image.new('L', max_image_dimensions, 0)
+    draw = ImageDraw.Draw(mask) 
+    draw.ellipse((0, 0) + max_image_dimensions, fill=255)
+
+    im = resize_image
+
+    resize_image = ImageOps.fit(im, mask.size, centering=(0.5, 0.5))
+    resize_image.putalpha(mask)
+
+    streamer_images.append(resize_image)
     resize_image = image.resize( image_dimensions ) 
-
     # test the computer locations along the ellipse
 
     def buttonPressed(streamer_name):
       print(f"{streamer_name} Pressed")
 
       index = home_order.index(streamer_name)
+      def new_index(index, offset):
+        index += offset
+        if index >= len(home_order):
+          # wrap around
+          index -= len(home_order)
+        elif(index < 0):
+          index += len(home_order)
+        return index
+      print(directional_arrows)
+      directional_arrows[0][0].configure(command = partial(buttonPressed, home_order[new_index(index, directional_arrows[0][1])]))
+      directional_arrows[1][0].configure(command = partial(buttonPressed, home_order[new_index(index, directional_arrows[1][1])]))
       for i in range(len(home_order)):
         # i is counting from bottom, counter countwise
         # curr is i offset by where we clicked on
@@ -114,20 +146,50 @@ def home(target):
         # cur button
         cur_button = streamer_buttons[i][0]
         image_dimensions = streamer_buttons[i][1]
+        mytext = streamer_buttons[i][2]
         # change image
         image = streamer_images[curr]
         resize_image = image.resize( image_dimensions ) 
         img =ImageTk.PhotoImage(resize_image)
         cur_button.configure(image=img)
         cur_button.image = img 
- 
+
         # change callback
         cur_button.configure(command = partial(buttonPressed, home_order[curr]))
+        cur_button.streamer_name = home_order[curr]
+        # change text
+        mytext["text"] = home_order[curr]
+        mytext["width"] = len(home_order[curr])
 
-         
-        
 
     img = ImageTk.PhotoImage(resize_image)
+    if (first):
+      # add left and right button
+      left = Image.open(f"Pics/home/left.png")
+      right = ImageOps.mirror(left)
+      right_image = ImageTk.PhotoImage(right)
+      left_image = ImageTk.PhotoImage(left)
+      left_offset = 1
+      right_offset = -1
+      right_button = ttk.Button(
+        slideshow_base,
+        command = partial(buttonPressed, home_order[home_order.index(streamer_name) +right_offset]),
+        image = right_image,  
+      )
+      left_button = ttk.Button(
+        slideshow_base,
+        command = partial(buttonPressed, home_order[home_order.index(streamer_name) +left_offset]),
+        image = left_image,  
+      )
+
+
+      left_button.place(x= image_x-image_max_x,y= image_y)
+      left_button.image = left_image
+      right_button.place(x= image_x + image_max_x,y= image_y)
+      right_button.image = right_image
+      directional_arrows.append((left_button,left_offset))
+      directional_arrows.append((right_button, right_offset))
+
 
     # btn = None
     btn = ttk.Button(
@@ -138,11 +200,21 @@ def home(target):
     )
     btn.streamer_name = streamer_name
     btn.image = img
-    btn.place(x=image_x, y=image_y, anchor=CENTER)
-    streamer_buttons.append((btn, image_dimensions))
+    btn.place(x=image_x, y=image_y+slideshow_offset_y, anchor=CENTER)
+    streamer_buttons.append((btn, image_dimensions, name_tage))
+
     
+    first = False
     streamer_index += 1
- 
+  # build the status box
+  status_box = Canvas(slideshow_base, width="25 vw", height="10vh")
+  status_box.pack(anchor=S)
+  cur_name_label = Label(status_box, text=home_order[0], width=len(home_order[0]), height=1)
+  cur_name_label.grid(row=0, column=0)
+  go_image = Image.open(f"Pics/home/go.png")
+  go_button = Button(status_box, command= partial(listFunctions.go, home_order[0]))
+
+
   ws.mainloop()
 
   
---
+++ listFunctions.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+import math
+from PIL import Image, ImageTk, ImageOps, ImageDraw
+
+from functools import partial
+
+def go():
+    # clear screen
+    # render bg image
+    # create table with image
+    
